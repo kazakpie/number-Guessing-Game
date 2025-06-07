ce035599e9b9b6c8db7768bd987c9c77e381999a
bolatkhaknazar <bolatkhaknazar@gmail.com>
2025-06-08 00:46:45 +0600

    bestResult method was added to Player class

--- numberGuessingGame/src/Player.java
+++ numberGuessingGame/src/Player.java
@@ -1,11 +1,25 @@
 public class Player {
 
     private String name;
+    private int score;
 
     public void setName(String n) {
         name = n;
     }
     public String getName () {
         return name;
+    }
+
+    public void setScore(int s) {score = s;}
+    public int getScore () {return score;}
+
+    public int bestResult(int num) {
+        if (this.getScore() > num) {
+            this.setScore(score);
+        }
+        else {
+            this.setScore(num);
+        }
+        return this.getScore();
     }
 }
